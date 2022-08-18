1d992d9660aee06ac1d8c112b957b68835558c28
alexNovik <alexnovik86@gmail.com>
2022-08-18 21:55:20 +0300

    Forth commit

--- plugins/modal.js
+++ plugins/modal.js
@@ -1,4 +1,8 @@
-function _createModalWindow() {
+function _createModalWindow(
+  title = "Modal title",
+  closable = true,
+  content = "Lorem ipsum dolor sit amet."
+) {
   const modalWindow = document.createElement("div");
   modalWindow.classList.add("modal");
   modalWindow.insertAdjacentHTML(
@@ -6,12 +10,12 @@ function _createModalWindow() {
     `<div class="modal__rearward">
         <div class="modal__window">
           <div class="modal__header">
-            <span class="modal__title">Modal title</span>
+            <span class="modal__title">${title}</span>
             <span class="modal__close">&times;</span>
           </div>
           <div class="modal__body">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>Lorem ipsum dolor sit amet.</p>
+            <p>${content}</p>
+            <p>${content}</p>
           </div>
           <div class="modal__footer">
             <button>Ok</button>
@@ -21,15 +25,24 @@ function _createModalWindow() {
       </div>`
   );
   document.body.appendChild(modalWindow);
+  if (!closable) {
+    const modalClose = document.querySelector(".modal__close");
+    modalClose.hidden = true;
+  }
   return modalWindow;
 }
 $.modal = function (options) {
   const ANIMATION_SPEED = 200;
-  const $modalWindow = _createModalWindow();
+  console.log(options["title"]);
+  const $modalWindow = _createModalWindow(
+    options["title"],
+    options["closable"],
+    options["content"]
+  );
   let closing = false;
   return {
     open() {
-      $modalWindow.classList.add("open");
+      !closing && $modalWindow.classList.add("open");
     },
     close() {
       closing = true;
